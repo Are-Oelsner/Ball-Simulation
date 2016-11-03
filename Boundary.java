d8a5024990a741507131cd5e9e7433a14e9822b6
areo279 <are.oelsner@richmond.edu>
2016-11-03 01:28:22 -0400

    Working Directory for Ball Simulation Code
    Based on Jory's second code dump

---
+++ Boundary.java
@@ -0,0 +1,29 @@
+//Name: Are Oelsner
+//My Boundary Class
+
+public class Boundary {
+	public double xmax;
+	public double xmin;
+	public double ymax; 
+	public double ymin;
+
+	public Boundary() {
+		xmax = 100;
+		xmin = 0;
+		ymax = 100;
+		ymin = 0;
+	}
+	
+	public Boundary(double _xmin, double _xmax, double _ymin, double _ymax) {
+		xmin = _xmin;
+		xmax = _xmax;
+		ymin = _ymin;
+		ymax = _ymax;
+	}
+	
+	public void drawBoundary(Boundary b) {
+		
+	public static void main(String[] args) {
+	}
+}
+
